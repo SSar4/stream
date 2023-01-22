59364fc52c1e439c72b0a0d2a925c8263c0b4688
j.a.s <aras.strong8@gmail.com>
2023-01-22 14:54:05 -0300

    feat: stream's video

--- server.js
+++ server.js
@@ -30,5 +30,38 @@ app.use('/memory', async function (req, res) {
     res.end(data);
   });
 })
+
+// stream's video
+app.get('/video', (req, res) => {
+  //caminho do video
+  const file = `./video/video.mp4`;
+  fs.stat(file, (err, stats) => {
+    if (err) {
+      console.log(err);
+      return res.status(404).end('<h1>Video Not found</h1>');
+    }
+    // Variáveis necessárias para montar o chunk header corretamente
+    const { range } = req.headers;
+    const { size } = stats;
+    const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
+    const end = size - 1;
+    const chunkSize = (end - start) + 1;
+    // Definindo headers de chunk
+    res.set({
+      'Content-Range': `bytes ${start}-${end}/${size}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunkSize,
+      'Content-Type': 'video/mp4'
+    });
+    // É importante usar status 206 - Partial Content para o streaming funcionar
+    res.status(206);
+    // Utilizando ReadStream do Node.js
+    // Ele vai ler um arquivo e enviá-lo em partes via stream.pipe()
+    const stream = fs.createReadStream(file, { start, end });
+    stream.on('open', () => stream.pipe(res));
+    stream.on('error', (streamErr) => res.end(streamErr));
+  });
+});
+
 app.listen(3000)
 
